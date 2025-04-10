ec726d4ab20fa28034e078308cc02be8df1b2d54
Arjuna Panji <arjuna.panji97@gmail.com>
2025-04-10 17:42:09 +0700

    update wisuda

--- resources/views/homepage/kegiatan/pendaftaran_wisuda.blade.php
+++ resources/views/homepage/kegiatan/pendaftaran_wisuda.blade.php
@@ -10,17 +10,7 @@
         <div class="space-y-4">
             <p class="font-normal text-gray-500 lg:text-lg text-justify dark:text-gray-400">
                 Bagi calon wisudawan <strong>diwajibkan untuk mendaftar secara online</strong> melalui form yang sudah disediakan, ijazah
-                diberikan langsung pada saat hari wisuda. Peserta wisuda adalah yang telah terjaring Yudisium dengan SK Rektor:
-            </p>
-
-            <ul class="list-disc pl-8 text-gray-500 lg:text-lg space-y-1">
-                @foreach($sk_rektor as $sk)
-                    <li>{{ $sk }}</li>
-                @endforeach
-            </ul>
-
-            <p class="font-normal text-gray-500 lg:text-lg text-justify dark:text-gray-400">
-                dan Ijazah sudah sampai di kantor Universitas Terbuka Daerah Jambi.
+                diberikan langsung pada saat hari wisuda. Peserta wisuda adalah yang telah terjaring Yudisium dan Ijazah sudah sampai di kantor Universitas Terbuka Daerah Jambi.
             </p>
         </div>
 
@@ -68,7 +58,7 @@
                         <time
                             class="block mb-2 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">{{$calon_wisuda->nim}}</time>
                         <p class="mb-4 text-base font-normal text-gray-500 dark:text-gray-400">Selamat, kamu telah
-                            terjaring yudisium masa {{$calon_wisuda->masa_yudisium}}.</p>
+                            terjaring yudisium masa {{$calon_wisuda->masa_yudisium}}. Silahkan cetak LIP Wisuda melalui tombol dibawah ini. <span class="text-red-600 text-sm italic">*(Jika tombol cetak LIP belum muncul pada laman aksi, silahkan menunggu paling lambat 3x24 Jam)</span></p>
                             <a href="https://aksi.ut.ac.id/alumni" target="_blank" class="inline-flex shadow items-center px-4 py-2 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:outline-none focus:ring-gray-100 focus:text-blue-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700 dark:focus:ring-gray-700">
                                   <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512" class="w-4 h-4 me-2.5" fill="currentColor"><path d="M64 0C28.7 0 0 28.7 0 64L0 448c0 35.3 28.7 64 64 64l256 0c35.3 0 64-28.7 64-64l0-288-128 0c-17.7 0-32-14.3-32-32L224 0 64 0zM256 0l0 128 128 0L256 0zM80 64l64 0c8.8 0 16 7.2 16 16s-7.2 16-16 16L80 96c-8.8 0-16-7.2-16-16s7.2-16 16-16zm0 64l64 0c8.8 0 16 7.2 16 16s-7.2 16-16 16l-64 0c-8.8 0-16-7.2-16-16s7.2-16 16-16zm16 96l192 0c17.7 0 32 14.3 32 32l0 64c0 17.7-14.3 32-32 32L96 352c-17.7 0-32-14.3-32-32l0-64c0-17.7 14.3-32 32-32zm0 32l0 64 192 0 0-64L96 256zM240 416l64 0c8.8 0 16 7.2 16 16s-7.2 16-16 16l-64 0c-8.8 0-16-7.2-16-16s7.2-16 16-16z"/></svg>
                                 Cetak LIP Wisuda
@@ -198,8 +188,8 @@
                             class="block mb-2 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">di konfirmasi
                             pada {{date('d/m/Y - H:i', strtotime($calon_wisuda->tgl_konfirmasi))}} WIB</time>
                         <p class="mb-4 text-base font-normal text-gray-500 dark:text-gray-400">Pendaftaran wisuda anda telah dikonfirmasi oleh UT Jambi.</p>
-                        <p class="mb-4 text-base font-normal text-gray-500 dark:text-gray-400">Silahkan bergabung kedalam WAG resmi Wisuda UT Jambi Periode II Tahun 2024 melalui tombol di bawah ini.</p>
-                            <a href="https://chat.whatsapp.com/IXLlEwWymz7K4BzVOmrOIl" target="_blank"
+                        <p class="mb-4 text-base font-normal text-gray-500 dark:text-gray-400">Silahkan bergabung kedalam WAG resmi Wisuda UT Jambi Periode I Tahun 2025 melalui tombol di bawah ini.</p>
+                            <a href="https://chat.whatsapp.com/DqJClLY57AWGXit3EwFIj0" target="_blank"
                                 class="inline-flex shadow items-center gap-2 px-4 py-2 text-sm font-medium text-white bg-green-500 border border-gray-200 rounded-lg hover:bg-green-700 hover:text-white focus:z-10 focus:ring-4 focus:outline-none focus:ring-gray-100 focus:text-white dark:bg-green-600 dark:text-white dark:border-gray-600 dark:hover:text-white dark:hover:bg-green-700 dark:focus:ring-gray-700">
                                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" class="w-5 h-5" fill="currentColor"><path d="M380.9 97.1C339 55.1 283.2 32 223.9 32c-122.4 0-222 99.6-222 222 0 39.1 10.2 77.3 29.6 111L0 480l117.7-30.9c32.4 17.7 68.9 27 106.1 27h.1c122.3 0 224.1-99.6 224.1-222 0-59.3-25.2-115-67.1-157zm-157 341.6c-33.2 0-65.7-8.9-94-25.7l-6.7-4-69.8 18.3L72 359.2l-4.4-7c-18.5-29.4-28.2-63.3-28.2-98.2 0-101.7 82.8-184.5 184.6-184.5 49.3 0 95.6 19.2 130.4 54.1 34.8 34.9 56.2 81.2 56.1 130.5 0 101.8-84.9 184.6-186.6 184.6zm101.2-138.2c-5.5-2.8-32.8-16.2-37.9-18-5.1-1.9-8.8-2.8-12.5 2.8-3.7 5.6-14.3 18-17.6 21.8-3.2 3.7-6.5 4.2-12 1.4-32.6-16.3-54-29.1-75.5-66-5.7-9.8 5.7-9.1 16.3-30.3 1.8-3.7 .9-6.9-.5-9.7-1.4-2.8-12.5-30.1-17.1-41.2-4.5-10.8-9.1-9.3-12.5-9.5-3.2-.2-6.9-.2-10.6-.2-3.7 0-9.7 1.4-14.8 6.9-5.1 5.6-19.4 19-19.4 46.3 0 27.3 19.9 53.7 22.6 57.4 2.8 3.7 39.1 59.7 94.8 83.8 35.2 15.2 49 16.5 66.6 13.9 10.7-1.6 32.8-13.4 37.4-26.4 4.6-13 4.6-24.1 3.2-26.4-1.3-2.5-5-3.9-10.5-6.6z"/></svg> WAG Wisuda UT Jambi</a>
                         {{-- <a href="#"
@@ -212,7 +202,7 @@
                                     d="M18 12h-2.55l-2.975 2.975a3.5 3.5 0 0 1-4.95 0L4.55 12H2a2 2 0 0 0-2 2v4a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2v-4a2 2 0 0 0-2-2Zm-3 5a1 1 0 1 1 0-2 1 1 0 0 1 0 2Z" />
                             </svg> QR Code Absensi</a> --}}
                     </li>
-                    @elseif(!$calon_wisuda->konfirmasi_lip && $calon_wisuda->bukti_pembayaran != null)
+                    @elseif(!$calon_wisuda->konfirmasi_lip && $calon_wisuda->bukti_pembayaran != null && $calon_wisuda->bukti_pembayaran != 'Request LIP')
                     <li class="mb-10 ms-6">
                         <span
                             class="absolute flex items-center justify-center w-6 h-6 bg-blue-100 text-blue-600 rounded-full -start-3 ring-8 ring-white dark:ring-gray-900 dark:bg-blue-900">
